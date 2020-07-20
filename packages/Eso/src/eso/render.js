b67637da734c7f2407e12d9e325b19b6333016ad
Hervé <hsaintmacary@2jprocess.com>
2020-07-20 14:04:34 +0200

    add events to render

--- packages/Eso/src/eso/render.js
+++ packages/Eso/src/eso/render.js
@@ -1,16 +1,17 @@
-import { o, api } from 'sinuous';
+import { o, api } from "sinuous";
 
 export function render(props) {
-  for (const p in props) {
-    if (!this.attributes[p]) {
-      this.attributes[p] = o(props[p]);
-      this.node && api.property(this.node, this.attributes[p], p);
-    } else {
-      console.log('this.attributes[p]', this.attributes, p);
-      this.attributes[p](props[p]);
-    }
-  }
+	console.log("this.current", props);
+	for (const p in props) {
+		if (!this.attributes[p]) {
+			this.attributes[p] = o(props[p]);
+			this.node && api.property(this.node, this.attributes[p], p);
+		} else {
+			this.attributes[p](props[p]);
+		}
+		console.log("this.attributes[%s]", p, this.attributes[p]());
+	}
 
-  console.log('attributes', this.attributes);
-  console.log('this.current', this.current);
+	console.log("attributes", this.attributes);
+	console.log("this.current", this.current);
 }
--- packages/Eso/src/index.js
+++ packages/Eso/src/index.js
@@ -1,58 +1,58 @@
-import { o } from 'sinuous';
-import { Eso } from './eso';
+import { o } from "sinuous";
+import { Eso } from "./eso";
 
-import { storeNodes } from './register/create-perso';
+import { storeNodes } from "./register/create-perso";
 
 class Toto extends Eso {
-  constructor(props) {
-    super(props);
-    this.revision.toto = {
-      update(props, state) {
-        console.log('(props, state)', props, state);
-        return props;
-      },
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.revision.toto = {
+			update(props, state) {
+				console.log("(props, state)", props, state);
+				return props;
+			},
+		};
+	}
 }
 const counter = o(0);
 
 const outer = {
-  classes: 'outer',
-  dynStyle: { color: 'red' },
-  onclick(e) {
-    console.log(this);
-  },
-  content: 'vat',
+	classes: "outer",
+	dynStyle: { color: "red" },
+	onclick: function (e) {
+		console.log(this);
+	},
+	content: "vat",
 };
 
 const inner = {
-  classes: 'inner',
-  content: counter,
+	classes: "inner",
+	content: counter,
 };
 
 const casting = {
-  outer: new Toto({ id: 'outer', perso: 'bloc', initial: outer }),
-  inner: new Eso({ id: 'inner', perso: 'bloc', initial: inner }),
+	outer: new Toto({ id: "outer", perso: "bloc", initial: outer }),
+	inner: new Eso({ id: "inner", perso: "bloc", initial: inner }),
 };
 
 const updatePerso = {
-  toto: 'tsoin-tsoin',
-  dynStyle: { color: '', 'font-weight': 'bold' },
-  classes: 'tontonton',
-  statStyle: { color: 'blue', 'font-size': '24px' },
-  'data-config': 'tintin',
-  content: storeNodes.get(casting.inner.uuid),
+	toto: "tsoin-tsoin",
+	dynStyle: { color: "", "font-weight": "bold" },
+	classes: "tontonton",
+	statStyle: { color: "blue", "font-size": "24px" },
+	"data-config": "tintin",
+	content: storeNodes.get(casting.inner.uuid),
 };
 
 const interval = setInterval(() => {
-  counter(counter() + 1);
-  if (counter() === 10) clearInterval(interval);
+	counter(counter() + 1);
+	if (counter() === 10) clearInterval(interval);
 }, 1000);
 
 document.body.append(storeNodes.get(casting.outer.uuid));
 
 setTimeout(() => {
-  casting.outer.update(updatePerso);
-  counter(counter() + 1);
-  if (counter() === 10) clearInterval(interval);
+	casting.outer.update(updatePerso);
+	counter(counter() + 1);
+	if (counter() === 10) clearInterval(interval);
 }, 1000);
