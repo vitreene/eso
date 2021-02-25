6c3387c4ee133983da021111b39b0426f6977a1c
vitreene <herve@vitreene.com>
2021-02-25 22:04:38 +0100

    merge stories  - persos

--- packages/Player/src/fetch/merge-stories.ts
+++ packages/Player/src/fetch/merge-stories.ts
@@ -1,16 +1,20 @@
 import { Story, StoryEntry } from '../../../types/Entries-types';
+import { Perso } from '../../../types/initial';
 
-export function mergeStories(_stories: StoryEntry[], _inherit: Story[]) {
+export function mergeStories(_stories: Story[], _inherit: Story[]) {
 	if (!_stories.length || !_inherit.length) return _stories;
 
 	const shared = new Map(Array.from(_inherit, (story) => [story.id, story]));
 	const stories = new Map(Array.from(_stories, (story) => [story.id, story]));
 
+	console.log('mergeStories', shared, stories);
+
 	for (const [id, _story] of stories) recMerge(id, _story);
 
 	// FIXME ne merge pas
-	function recMerge(id, _story) {
+	function recMerge(id: string, _story: StoryEntry) {
 		const story = stories.get(_story.extends) || shared.get(_story.extends);
+
 		if (!story) return _story;
 		const _proto = recMerge(_story.extends, story);
 		if (_proto) {
@@ -31,12 +35,21 @@ const merge = {
 		const persos = this.persos(_proto.persos, _story.persos);
 		const eventimes = this.eventimes(_proto.eventimes, _story.eventimes);
 
-		return Object.assign(_story, persos, eventimes);
+		return Object.assign(
+			{},
+			_proto,
+			_story,
+			{ eventimes },
+			persos && persos.length && { persos }
+		);
 	},
-	persos(_proto, _story) {
-		return _story;
+	persos(_protos, _persos: Perso[]) {
+		const persos = new Map(Array.from(_persos, (perso) => [perso.id, perso]));
+		for (const p of _protos) !persos.has(p.id) && persos.set(p.id, p);
+
+		return Array.from(persos.values());
 	},
 	eventimes(_proto, _story) {
-		return _story;
+		return _proto;
 	},
 };
